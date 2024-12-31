3c452e4c975d5468613c6978e685225b31037491
junjiezh <zjj476778843@gmail.com>
2024-12-31 18:05:43 +0800

    use ZREVRANGE rather than ZDIFF to decrease the cost of getFeed operation

--- backend/src/main/resources/fetchNewsInCommunity.lua
+++ backend/src/main/resources/fetchNewsInCommunity.lua
@@ -1,14 +1,15 @@
-local community = KEYS[1];
-local user = KEYS[2];
+local community = KEYS[1]
+local user = KEYS[2]
 
-local diff = redis.call('ZDIFF', 2, community, user, 'WITHSCORES');
+local diff = redis.call("ZREVRANGE", community, 0, 49, "WITHSCORES")
 
 if #diff > 0 then
-    local news = {}
-    for i = 1, #diff, 2 do
-        table.insert(news, diff[i + 1])
-        table.insert(news, diff[i])
-    end
-    redis.call('ZADD', user, 'NX', unpack(news));
+	local news = {}
+	for i = 1, #diff, 2 do
+		table.insert(news, diff[i + 1])
+		table.insert(news, diff[i])
+	end
+	redis.call("ZADD", user, "NX", unpack(news))
 end
 return true
+
